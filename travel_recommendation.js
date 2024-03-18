7de840b5a7b06a2390bf0b3dd04e6ddbfcf3b267
Tom Johnston <tom.johnston.2017@live.rhul.ac.uk>
2024-03-17 20:06:07 -0400

    can isolate keywords in json using search bar

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -8,4 +8,23 @@ function resetForm() {
     searchResults.classList.add("invisible");
 }
 
+function searchDestination() {
+    const input = searchInput.value.toLowerCase();
+    searchResults.innerHTML = '';
+
+    fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            if (input == "country" || input == "countries") {
+                resultsFinder = data.countries;
+            } else if (input == "temple" || input == "temples") {
+                resultsFinder = data.temples;
+            } else if (input == "beach" || input == "beaches") {
+                resultsFinder = data.beaches;
+            }
+            console.log(resultsFinder);
+        });
+}
+
 btnReset.addEventListener("click", resetForm);
+btnSearch.addEventListener("click", searchDestination);
