fcc98cd80f292703509e5cb0877f5059320d4661
Tom Johnston <tom.johnston.2017@live.rhul.ac.uk>
2024-03-17 21:23:40 -0400

    trying to decode json to make it display

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -17,12 +17,39 @@ function searchDestination() {
         .then(data => {
             if (input == "country" || input == "countries") {
                 resultsFinder = data.countries;
+                for (country in resultsFinder) {
+                    console.log('country.cities');
+                    console.log(country.cities);
+                    console.log('resultsFinder');
+                    console.log(resultsFinder);
+                    console.log();
+                    for (city in resultsFinder) {
+                        console.log('city');
+                        console.log(city.cities);
+                        searchResults.innerHTML += `<li class="searchResultsContent">`;
+                        searchResults.innerHTML += `<img src="${country.imageUrl}" class="searchResultsImages">`;
+                        searchResults.innerHTML += `<p><h1>${country.name}</h1></p>`;
+                        searchResults.innerHTML += `<p>${country.description}</p>`;
+                        searchResults.innerHTML += `<button class="bookNowBtn">Visit</button>`;
+                        searchResults.innerHTML += `</li>`;
+                    }
+                }
+                searchResults.classList.remove("invisible");
             } else if (input == "temple" || input == "temples") {
                 resultsFinder = data.temples;
             } else if (input == "beach" || input == "beaches") {
                 resultsFinder = data.beaches;
             }
-            console.log(resultsFinder);
+
+ /*           for (result in resultsFinder) {
+                console.log(result);
+                searchResults.innerHTML += `<li class="searchResultsContent">`;
+                searchResults.innerHTML +=temples `<img src="${result.imageUrl}" class="searchResultsImages">`;
+                searchResults.innerHTML += `<p><h1>${result.name}</h1></p>`;
+                searchResults.innerHTML += `<p>${result.description}</p>`;
+                searchResults.innerHTML += `<button class="bookNowBtn">Visit</button>`;
+                searchResults.innerHTML += `</li>`;
+            }*/
         });
 }
 
